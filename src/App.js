4cc436dbcab7bf7b2dbd7a3777d0101cb6ed01be
mimuruth-msft <michaelmmckenzie11@gmail.com>
2024-05-23 11:41:05 -0500

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div>
+      <h1>Hello {value}</h1>
+      <p>Welcome to our demo website!</p>
+      <p>This is a simple React application.</p>
+      <p>Feel free to explore the features.</p>
+      <ul>
+        <li>Feature 1</li>
+        <li>Feature 2</li>
+        <li>Feature 3</li>
+      </ul>
+      <p>Thank you for visiting!</p>
+    </div>
+  );
 }
 
 export default App;
+
